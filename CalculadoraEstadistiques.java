b0fa89f50b7cd1be9db3b9801c99c57be096bfac
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-05-02 09:11:03 +0200

    Add files via upload

---
+++ CalculadoraEstadistiques.java
@@ -0,0 +1,15 @@
+package notes;
+
+class CalculadoraEstadistiques{
+    /*Importo las notas ns como*/
+    
+    public void calcularMitjana(double[] notes){
+        
+    }
+    public void calcularMaxim(double[] notes){
+        
+    }
+    public void calcularMinim(double[] notes){
+        
+    }
+}
---
+++ GestorNotes.java
@@ -0,0 +1,15 @@
+package notes;
+
+class GestorNotes{
+    private String nomAlumne; 
+    private double[] notes;
+    private String nomAlumne;
+
+    public void registrarNotes(String nomAlumne, double[] notes){
+        
+    }
+
+    public void obtenirNotes(String nomAlumne) {
+
+    }
+}
